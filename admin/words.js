6f971d2854f3d355c65452ae3395f77e9fb1e85f
ldittmar <ldittmar.privat@gmail.com>
2021-09-28 11:58:54 +0200

    First commit

---
+++ admin/words.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"stockmarket adapter settings": {
+		"en": "Adapter settings for stockmarket",
+		"de": "Adaptereinstellungen für stockmarket",
+		"ru": "Настройки адаптера для stockmarket",
+		"pt": "Configurações do adaptador para stockmarket",
+		"nl": "Adapterinstellingen voor stockmarket",
+		"fr": "Paramètres d'adaptateur pour stockmarket",
+		"it": "Impostazioni dell'adattatore per stockmarket",
+		"es": "Ajustes del adaptador para stockmarket",
+		"pl": "Ustawienia adaptera dla stockmarket",
+		"zh-cn": "stockmarket的适配器设置"
+	},
+	"ownStocks": {
+		"en": "Your favorite stocks",
+		"de": "Deine gewünschten Aktien",
+	},
+	"ownStocks_label": {
+		"en": "Stockids (comma separated)",
+		"de": "Aktiennummer (mit Komma trennen)",
+	},
+	"ownStocks_info": {
+		"en": "Enter your own commands, which you entered in the GetAdmin software in column Command. No spaces please. Separate multiple values with comma.<br>Leave this field empty, if you have not (yet) defined any commands.",
+		"de": "Hier die Commands eintragen, die du in der GetAdmin-Software in der Spalte Commands definiert hast. Bitte ohne Leerzeichen. Mehrere Werte mit Komma trennen.<br>Falls du (noch) keine eigenen Commands definiert hast, lässt du das einfach leer.",
+	},
+	"apikey": {
+		"en": "Your API Key",
+		"de": "Dein API Key",
+	},
+	"apikey_label": {
+		"en": "Insert your API Key",
+		"de": "API Key hier eintragen",
+	},
+	"apikey_info": {
+		"en": "Please check <a href=\"https://www.alphavantage.co/support/#api-key\">https://www.alphavantage.co/support/#api-key</a>, to get your own API Key",
+		"de": "Bitte besorge dir deinen eigenen API Key unter der folgenden Adresse: <a href=\"https://www.alphavantage.co/support/#api-key\">https://www.alphavantage.co/support/#api-key</a>",
+	}
+};
---
+++ main.js
@@ -0,0 +1,127 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.17.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+
+// Load your modules here, e.g.:
+const https = require("https");
+
+
+class Stockmarket extends utils.Adapter {
+
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: "stockmarket",
+		});
+		this.on("ready", this.onReady.bind(this));
+		// this.on("message", this.onMessage.bind(this));
+		this.on("unload", this.onUnload.bind(this));
+	}
+
+	async onReady() {
+		this.log.info("initialize stock market adapter"); 
+
+		const apikey = this.config.apiKey;
+		if(apikey == "" || apikey == "string") {
+			this.log.error("No API Key set. Please edit your adapter settings and restart this adapter!");
+			this.terminate("No Api Key");
+			return;
+		}
+
+		this.log.info("APIKEY is set: '" + this.config.apiKey + "'");
+
+		this.log.info("stocks to check: " + this.config.ownStocks);
+		const stocks = this.config.ownStocks.split(",");
+
+		stocks.forEach(stock => {
+			stock = stock.replace(" ", "");
+			const url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + stock + "&interval=5min&apikey=" + apikey;
+			this.log.info("checking stock for: " + stock + " with URL: " + url);
+			
+			https.get(url, (resp) => {
+				const data = [];
+				resp.on("data", d => data.push(d));
+				resp.on("end", () => {
+					let jsonstring;
+					try {
+						jsonstring = JSON.parse(data.join(""));
+					} catch (err) {
+						this.log.error("Parsing JSON Error: " + err.message);
+						this.terminate("Parsing JSON Error");
+						return;		
+					}
+				
+					if(!("Time Series (5min)" in jsonstring)) {
+						this.log.error("JSON Object is wrong");
+						this.terminate("JSON Object is wrong");
+						return;	
+					}
+					
+					for (const i in jsonstring["Time Series (5min)"]) {
+						//("2019-11-18 16:00:00" in jsonstring["Time Series (5min)"]) ? this.log.info("Super") : this.log.info("Nein");
+						for (const e in jsonstring["Time Series (5min)"][i]) {
+							const stateName = stock + "." + e.replace(". ", "");
+							let unit = "USD";
+							if(e.replace(". ", "") == "5volume") { unit = ""; }
+
+							this.setObjectNotExists(stateName, {
+								type: "state",
+								common: {
+									name: "Last Value for " + e,
+									type: "string",
+									role: "text",
+									unit: unit,
+									read: true,
+									write: false
+								},
+								native: {}
+							});
+
+							this.setState(stateName, jsonstring["Time Series (5min)"][i][e]);
+						}
+						break;
+					}					
+				});
+	
+			}).on("error", (err) => {
+				this.log.error("Error: " + err.message);
+				this.terminate("HTTP Request Error");
+				return;
+			});
+		});
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			this.log.info("cleaned everything up...");
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Stockmarket(options);
+} else {
+	// otherwise start the instance directly
+	new Stockmarket();
+}
